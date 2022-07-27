7c8eb060ceb7bbe853f69232ad2c52948123e941
Nerz <adrian.dufac@gmail.com>
2022-07-27 22:23:49 +0200

    ajout d'un produit controller, deplacement de la methode dirigeant vers la twig d'ajout de produit. Création d'un twig pour voir les détails d'un produit. Création de la méthode dirigeant vers la page de détails

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -20,31 +20,4 @@ class MainController extends AbstractController
         return $this->render('main/home.html.twig',["produits"=>$produits]);
     }
 
-    /**
-     * @Route("/ajoutProduit",name="main_add")
-     */
-    public function test(Request $request,EntityManagerInterface $entityManager){
-
-        $produit = new Produit();
-        //creation du formulaire
-        $produitForm = $this->createForm(ProduitType::class,$produit);
-
-        $produitForm->handleRequest($request);
-        //si on submit le formulaire
-        if($produitForm->isSubmitted()){
-            //ajout de la produit en base
-            $produit->setDateAjout(new \DateTime('@'.strtotime('now')));
-            $entityManager->persist($produit);
-            $entityManager->flush();
-
-            $this->addFlash('success', 'produit ajouté!');
-            //on affiche la liste des produits
-            return $this->redirectToRoute('main_home');
-        }
-
-        //on envoit le formulaire a la page d'ajout de category
-        return $this->render('main/ajout.html.twig',['produitForm' =>$produitForm->createView()]);
-
-    }
-
 }
---
+++ src/Controller/ProduitController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Produit;
+use App\Form\ProduitType;
+use App\Repository\ProduitRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProduitController extends AbstractController
+{
+
+    /**
+     * @Route("/ajoutProduit",name="produit_add")
+     */
+    public function test(Request $request,EntityManagerInterface $entityManager){
+
+        $produit = new Produit();
+        //creation du formulaire
+        $produitForm = $this->createForm(ProduitType::class,$produit);
+
+        $produitForm->handleRequest($request);
+        //si on submit le formulaire
+        if($produitForm->isSubmitted()){
+            //ajout de la produit en base
+            $produit->setDateAjout(new \DateTime('@'.strtotime('now')));
+            $entityManager->persist($produit);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'produit ajouté!');
+            //on affiche la liste des produits
+            return $this->redirectToRoute('main_home');
+        }
+
+        //on envoit le formulaire a la page d'ajout de category
+        return $this->render('produit/ajoutproduit.html.twig',['produitForm' =>$produitForm->createView()]);
+
+    }
+
+    /**
+     * @Route("/produitDetails/{id}", name="produit_details")
+     */
+    public function details(int $id,ProduitRepository $wishRepository): Response
+    {
+        $produit = $wishRepository->find($id);
+        return $this->render('produit/details.html.twig',["produit" => $produit]);
+    }
+
+}
