8c47370c92b7c187af5db9df9fd9d85266ea1399
Nerz <adrian.dufac@gmail.com>
2022-07-27 21:48:50 +0200

    Ajout du formulaire ProduitType pour les produits, mise en place du formulaire dans le twig d'acceuil et dans le twig d'ajout de produit

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,7 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Produit;
+use App\Form\ProduitType;
+use App\Repository\ProduitRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
@@ -10,15 +15,35 @@ class MainController extends AbstractController
     /**
      * @Route("/",name="main_home")
      */
-    public function home(){
-        return $this->render('main/home.html.twig');
+    public function home(ProduitRepository $produitRepository){
+        $produits = $produitRepository->findAll();
+        return $this->render('main/home.html.twig',["produits"=>$produits]);
     }
 
     /**
      * @Route("/ajoutProduit",name="main_add")
      */
-    public function test(){
-        return $this->render('main/ajout.html.twig');
+    public function test(Request $request,EntityManagerInterface $entityManager){
+
+        $produit = new Produit();
+        //creation du formulaire
+        $produitForm = $this->createForm(ProduitType::class,$produit);
+
+        $produitForm->handleRequest($request);
+        //si on submit le formulaire
+        if($produitForm->isSubmitted()){
+            //ajout de la produit en base
+            $produit->setDateAjout(new \DateTime('@'.strtotime('now')));
+            $entityManager->persist($produit);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'produit ajouté!');
+            //on affiche la liste des produits
+            return $this->redirectToRoute('main_home');
+        }
+
+        //on envoit le formulaire a la page d'ajout de category
+        return $this->render('main/ajout.html.twig',['produitForm' =>$produitForm->createView()]);
 
     }
 
---
+++ src/Form/ProduitType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Category;
+use App\Entity\Produit;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProduitType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('libelle')
+            ->add('reference')
+            ->add('prix')
+            ->add('stock')
+            ->add('categorie',EntityType::class,['label'=>'Category','class'=>Category::class,'choice_label'=>'libelle'])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Produit::class,
+        ]);
+    }
+}
